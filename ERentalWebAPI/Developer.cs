5f3f2f589dd1dd59e6acb69369358ec46b24bb1a
Pankaj <Pankajraopatil@gmail.com>
2023-01-10 11:44:24 +0530

    Sample developer.cs file added for batch

---
+++ ERentalWebAPI/Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERentalWebAPI
+{
+    /// <summary>
+    /// Developer entity class created by Pankaj
+    /// </summary>
+    public class Developer
+    {
+        public int DevID { get; set; }
+        public string DevName { get; set; }
+    }
+}
